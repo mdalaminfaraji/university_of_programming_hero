a0f6f0372d69ab8b4e7d30bafc7c383aff099a79
mdalaminfaraji <alaminice1617@gmail.com>
2023-12-11 08:11:25 +0600

    acdemic department

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -14,6 +14,22 @@ const academicFacultySchema = new Schema<TAcademicFaculty>(
   },
 );
 
+academicFacultySchema.pre('save', async function (next) {
+  const isFacultyExist = await AcademicFaculty.findOne({ name: this.name });
+  if (isFacultyExist) {
+    throw new Error('Faculty is already exist !');
+  }
+  next();
+});
+
+academicFacultySchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicFaculty.findOne(query);
+  if (!isDepartmentExist) {
+    throw new Error('This faculty does not exist !');
+  }
+  next();
+});
 export const AcademicFaculty = model<TAcademicFaculty>(
   'AcademicFaculty',
   academicFacultySchema,
